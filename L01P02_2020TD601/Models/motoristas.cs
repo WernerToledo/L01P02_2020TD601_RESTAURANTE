5661bad33dd8812b6b0d85229e24ea08b5e01d82
Alejandro Toledo <88223531+WernerToledo@users.noreply.github.com>
2023-04-22 13:25:11 -0600

    Agregar archivos de proyecto.

---
+++ L01P02_2020TD601/Models/motoristas.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace L01P02_2020TD601.Models
+{
+    public class motoristas
+    {
+        [Key]
+        public int motoristaId { get; set; }
+        public string nombreMotorista { get; set; }
+    }
+}
---
+++ L01P02_2020TD601/Models/motoristasContext.cs
@@ -0,0 +1,13 @@
+﻿using L01_2020TD601.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace L01P02_2020TD601.Models
+{
+    public class motoristasContext : DbContext
+    {
+        public motoristasContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<motoristas> motoristas { get; set; }
+    }
+}
